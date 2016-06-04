0372f3e5870225176f1d46560ed797512b7e486f
Djordje Stojanovic <djordje.stojanovic@ursus.rs>
2016-06-04 02:09:39 +0200

    Added shields and modified the release script.
    Signed-off-by: Djordje Stojanovic <djordje.stojanovic@ursus.rs>

--- .travis/release.sh
+++ .travis/release.sh
@@ -4,6 +4,7 @@ TRAVIS="${TRAVIS:-""}"
 TRAVIS_PULL_REQUEST="${TRAVIS_PULL_REQUEST:-"false"}"
 GITHUB_USER="${GITHUB_USER:-""}"
 GITHUB_TOKEN="${GITHUB_TOKEN:-""}"
+COMMIT_AUTHOR_EMAIL="${COMMIT_AUTHOR_EMAIL:-"$(git config user.email)"}"
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     mvn release:prepare && mvn release:perform
@@ -18,6 +19,15 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
                 fi
             fi
         fi
+        if [[ $- == *i* ]]; then
+            read -p "Are you sure? " -n 1 -r
+            echo    # (optional) move to a new line
+            if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+                exit 0
+            fi
+        fi
+        git config user.name "Travis CI"
+        git config user.email "$COMMIT_AUTHOR_EMAIL"
         git push origin
     fi
 fi
--- README.md
+++ README.md
@@ -3,6 +3,7 @@ commons
 
 Ursus Software Common Libraries
 -------------------------------
+[![Build Status](https://travis-ci.org/ursus-software/commons.svg?branch=master)](https://travis-ci.org/ursus-software/commons) [![Code Climate](https://codeclimate.com/github/ursus-software/commons/badges/gpa.svg)](https://codeclimate.com/github/ursus-software/commons) [![Test Coverage](https://codeclimate.com/github/ursus-software/commons/badges/coverage.svg)](https://codeclimate.com/github/ursus-software/commons/coverage)
 
 #### Modules
 * spring-logging
